dc113ca015630d0c8ea03fd1a2ae7a5b61e55737
piape <pip002@edu.zealand.dk>
2026-08-02 21:53:48 +0200

    tilføjet link

--- index.php
+++ index.php
@@ -17,7 +17,13 @@ require "settings/init.php";
     <meta name="copyright" content="Information om copyright">
     
     <link href="css/styles.css" rel="stylesheet" type="text/css">
-    
+
+    <!-- Font Awesome ikoner -->
+    <script src="https://kit.fontawesome.com/737b386bab.js" crossorigin="anonymous"></script>
+
+    <!-- Bootstraps ikoner -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="s
+
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
