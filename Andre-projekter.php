5c1ce17d1387211227f7812c33c8a5e0e5a3976b
piape <pip002@edu.zealand.dk>
2026-08-08 20:31:32 +0200

    oprette nye filer

--- Andre-projekter.php
+++ Andre-projekter.php
@@ -5,12 +5,13 @@
 
 require "settings/init.php";
 ?>
+
 <!DOCTYPE html>
 <html lang="da">
 <head>
     <meta charset="utf-8">
 
-    <title>Pia's_Portfolio</title>
+    <title>Andre projekter</title>
 
     <meta name="robots" content="All">
     <meta name="author" content="Udgiver">
@@ -18,13 +19,42 @@ require "settings/init.php";
 
     <link href="css/styles.css" rel="stylesheet" type="text/css">
 
+    <!-- Font Awesome ikoner -->
+    <script src="https://kit.fontawesome.com/737b386bab.js" crossorigin="anonymous"></script>
+
+    <!-- Bootstraps ikoner -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="s
+
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
 <body>
 
-<h1>Det virker 🥳</h1>
+<header class="sub-hero-header">
+    <img src="img/billeder/hero_img.png" alt="Hero baggrund" class="sub-hero-bg-img">
 
+    <!-- Canvas til fysik-baserede bobler -->
+    <canvas id="bubbleCanvas"></canvas>
+
+    <nav class="navbar">
+        <div class="logo">
+            <img src="img/logo/e5c3a926-0a69-4233-8852-ee861d1e8d96-1.png" alt="PP logo">
+        </div>
+    </nav>
+
+    <!-- Centreret layout -->
+    <div class="sub-hero-layout">
+        <div class="sub-hero-text">
+            <h1>ANDRE PROJEKTER</h1>
+        </div>
+
+        <div class="sub-hero-img-box">
+            <img src="img/billeder/hero-target.png" alt="Andre Projekter" class="sub-hero-img">
+        </div>
+    </div>
+</header>
+
+<script src="index_js.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
